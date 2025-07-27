d61e552de438139f71c863ae75ebf27b75b74d89
Shruti Gupta <142827513+ShrutiGupta37@users.noreply.github.com>
2025-07-27 23:52:30 +0530

    Update create_memory_for_llm.py

--- week-8/create_memory_for_llm.py
+++ week-8/create_memory_for_llm.py
@@ -1,15 +1,16 @@
+"""
+step 1: load raw PDF(s)
+step 2: Create Chunks
+step 3: Create Vector Embeddings
+step 4: Store embeddings in FAISS
+"""
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
-## Uncomment the following files if you're not using pipenv as your virtual environment manager
-#from dotenv import load_dotenv, find_dotenv
-#load_dotenv(find_dotenv())
-
-
-# Step 1: Load raw PDF(s)
-DATA_PATH="data/"
+# Step 1
+DATA_PATH="MediBot\data"
 def load_pdf_files(data):
     loader = DirectoryLoader(data,
                              glob='*.pdf',
@@ -19,10 +20,10 @@ def load_pdf_files(data):
     return documents
 
 documents=load_pdf_files(data=DATA_PATH)
-#print("Length of PDF pages: ", len(documents))
+print("Length of PDF pages: ", len(documents))
 
 
-# Step 2: Create Chunks
+# Step 2
 def create_chunks(extracted_data):
     text_splitter=RecursiveCharacterTextSplitter(chunk_size=500,
                                                  chunk_overlap=50)
@@ -30,17 +31,17 @@ def create_chunks(extracted_data):
     return text_chunks
 
 text_chunks=create_chunks(extracted_data=documents)
-#print("Length of Text Chunks: ", len(text_chunks))
+print("Length of Text Chunks: ", len(text_chunks))
 
-# Step 3: Create Vector Embeddings 
 
+# Step 3
 def get_embedding_model():
     embedding_model=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     return embedding_model
 
 embedding_model=get_embedding_model()
 
-# Step 4: Store embeddings in FAISS
+# Step 4
 DB_FAISS_PATH="vectorstore/db_faiss"
 db=FAISS.from_documents(text_chunks, embedding_model)
 db.save_local(DB_FAISS_PATH)
